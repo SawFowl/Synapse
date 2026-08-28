e5d42dbab194e71f52ddbb145ec8258afbdcd9ef
Иван <StalkerIv@yandex.ru>
2026-08-28 20:02:22 +0300

    Update Sudo.java

--- synapse-plugin/src/main/java/sawfowl/synapse/commands/Sudo.java
+++ synapse-plugin/src/main/java/sawfowl/synapse/commands/Sudo.java
@@ -6,6 +6,7 @@ import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.Player;
 
 import sawfowl.synapse.Permissions;
+import sawfowl.synapse.api.Synapse;
 import sawfowl.synapse.api.commands.SynapseBrigadierCommand;
 import sawfowl.synapse.api.exceptions.CommandException;
 
@@ -17,6 +18,15 @@ public class Sudo extends AbstractCommand {
 		String spoofChatInput = command.getStringArgument(context, "SpoofChatInput").get();
 		if(context.getSource() instanceof Player p && player.hasPermission(Permissions.SUDO_IGNORE)) exception(getCommands(p).getSudo().getFail(player));
 		context.getSource().sendMessage(getCommands(context.getSource()).getSudo().getSuccess(player, spoofChatInput));
+		if(spoofChatInput.startsWith("/")) {
+			String c = (spoofChatInput.contains(" ") ? spoofChatInput.split(" ")[0] : spoofChatInput).replaceFirst("/", "");
+			if(Synapse.getProxy().getCommandManager().hasCommand(c, player)) {
+				c = null;
+				Synapse.getProxy().getCommandManager().executeAsync(player, spoofChatInput.replaceFirst("/", ""));
+				return command.success();
+			}
+			c = null;
+		}
 		player.spoofChatInput(spoofChatInput);
 		return command.success();
 	}
